04a9a30d3a51fa8b7ea3db78696dbe69b62b4e39
elad1478 <50262518+elad1478@users.noreply.github.com>
2019-07-02 17:07:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ app.get("/", function(req, res){
     app.render("index.html");
 });
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 app.listen(3000,  function(){
     console.log("the server has started");
  });
