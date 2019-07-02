6a4ae674c95eb7a37564abd38d37ae53cdfceb93
elad1478 <50262518+elad1478@users.noreply.github.com>
2019-07-02 17:14:50 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var express= require("express");
+
+app.get("/", function(req, res){
+    app.render("index.html");
+});
+
+app.listen(3000,  function(){
+    console.log("the server has started");
+ });
