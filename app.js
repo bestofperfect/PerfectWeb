b3a1ebc6ce2b7631146034ec937c11541c0fe447
elad1478 <50262518+elad1478@users.noreply.github.com>
2019-07-02 17:53:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 var express= require("express");
-
+var app=express();
+app.use(express.static("views"));
+// app.set('view engine', 'html');
 app.get("/", function(req, res){
-    app.render("index.html");
+    res.render("index.html");
 });
 
 app.listen(3000,  function(){
